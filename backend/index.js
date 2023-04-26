1dd079ae1bdc52d6da7cc8ef0c65e40b18e91bb0
Warisul Imam <57336955+leadersheir@users.noreply.github.com>
2023-04-26 23:58:31 +0600

    bugfix: changed `cors` with `cors()` in index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ morgan.token('post-content', (req, res) => {
 })
 
 app.use(express.static('build'))
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-content'))
 
