3e97cd7b1502f9d0dadbb860e66f0b5bcd6420c4
Warisul Imam <warisulimam@gmail.com>
2023-04-26 23:00:27 +0600

    added cors and static middleware

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -8,8 +9,11 @@ morgan.token('post-content', (req, res) => {
     if (req.method === 'POST') return JSON.stringify(req.body)
 })
 
+app.use(express.static('build'))
+app.use(cors)
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-content'))
+
 
 let persons = [
     { 
