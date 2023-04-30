1042501a770b0fdec67b43793cb8aeb905532957
Warisul Imam <warisulimam@gmail.com>
2023-04-30 23:36:28 +0600

    removed frontend

--- frontend/src/components/Filter.js
+++
@@ -1,7 +0,0 @@
-const Filter = ({ filterText, handleFilterTextChange }) => (
-    <>
-        filter shown with <input value={filterText} onChange={handleFilterTextChange} />
-    </>
-)
-
-export default Filter
--- frontend/src/components/Person.js
+++
@@ -1,5 +0,0 @@
-const Person = ({ person, handleDeletePerson }) => (
-    <p>{person.name} {person.number} <button onClick={() => handleDeletePerson(person)}>delete</button></p>
-)
-
-export default Person
